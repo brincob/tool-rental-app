959447bd789e4e04dff4168bd4d2ca6020aff034
Brian Banda <bandab45@gmail.com>
2024-10-24 05:46:53 +0200

    Create next.config.js

---
+++ src /next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    styledComponents: true, // Enable styled-components support, if needed
+  },
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
